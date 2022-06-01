3345706d7466792f459027e4bc1d2ad65d24a778
Keith Billings <keith.billings@brandlive.com>
2022-05-31 23:42:06 -0700

    style seach bar and move copy button

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,8 @@ export default function Home() {
 	return (
 		<div className="home-page">
 			<h1 className="home-page__title">What&apos;s My...</h1>
-			<CopyButton whatsMyData={whatsMyData} />
 			<SearchBar />
+			<CopyButton whatsMyData={whatsMyData} />
 			<WindowInfo setWhatsMyData={setWhatsMyData} />
 			<ExternalLinks />
 		</div>
